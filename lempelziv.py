672328cd220916bb258ead5c6a3b7e97e06f7657
Daniel Nagy <daniel.nagy@durham.ac.uk>
2019-02-03 16:15:02 +0000

    Make compression return a bitarray, change test case

--- lempelziv.py
+++ lempelziv.py
@@ -1,5 +1,6 @@
 """ Digital Communication - Lempel-Ziv compression """
 
+import bitarray
 
 class LempelZiv():
     """ Class implementing LZ77 coding """
@@ -19,9 +20,14 @@ class LempelZiv():
 
 # Function just for getting the algorithm right, will move to class once done
 def compress(message):
+    result = bitarray.bitarray()
+
     index = 0
     n = len(message)
     W = L = n
+
+    d_len = (W - 1).bit_length()
+    l_len = (L - 1).bit_length()
 
     while index < n:
         window = message[max(index - W, 0):index]
@@ -41,9 +47,35 @@ def compress(message):
         d = window[::-1].index(substring[::-1]) + l if l > 0 else 0
 
         # Just print the encoding info
-        print((d, l, next_sym))
+        # print((d, l, next_sym))
+
+        bin_d = bin(d)[2:]
+        bin_l = bin(l)[2:]
+
+        result += '0' * (d_len - len(bin_d)) + bin_d
+        result += '0' * (l_len - len(bin_l)) + bin_l
+
+        bin_next_sym = bitarray.bitarray()
+
+        if next_sym:
+            bin_next_sym.fromstring(next_sym)
+            result += bin_next_sym
+        else:
+            result += '11111111'
 
         index += l + 1
 
-# Test case from lecture slides
-compress('ABRACADABRA')
+    return result
+
+
+if __name__ == '__main__':
+    # Test case from lecture slides
+    test = "Peter Piper picked a peck of pickled peppers; A peck of pickled peppers Peter Piper picked;If Peter Piper picked a peck of pickled peppers,Where's the peck of pickled peppers Peter Piper picked?"
+
+    uncompressed = bitarray.bitarray()
+    uncompressed.fromstring(test)
+
+    compressed = compress(test)
+
+    print(f'Uncompressed length: {uncompressed.length()}')
+    print(f'Compressed length:   {compressed.length()}')
