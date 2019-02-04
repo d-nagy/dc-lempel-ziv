ea68d9796a083af6d10e5cf51f20e013ef9034bd
Daniel Nagy <daniel.nagy@durham.ac.uk>
2019-02-04 00:06:58 +0000

    Add decompression development function

--- lempelziv.py
+++ lempelziv.py
@@ -19,12 +19,11 @@ class LempelZiv():
 
 
 # Function just for getting the algorithm right, will move to class once done
-def compress(message):
+def compress(message, W, L):
     result = bitarray.bitarray()
 
     index = 0
     n = len(message)
-    W = L = n
 
     d_len = (W - 1).bit_length()
     l_len = (L - 1).bit_length()
@@ -58,7 +57,7 @@ def compress(message):
         bin_next_sym = bitarray.bitarray()
 
         if next_sym:
-            bin_next_sym.fromstring(next_sym)
+            bin_next_sym.frombytes(next_sym.encode())
             result += bin_next_sym
         else:
             result += '11111111'
@@ -68,14 +67,56 @@ def compress(message):
     return result
 
 
+# Function for getting the decoding algorithm right, will move to class
+def decompress(encoded, W, L):
+    message = ''
+    index = 0
+
+    d_len = (W - 1).bit_length()
+    l_len = (L - 1).bit_length()
+
+    step = d_len + l_len + 8
+
+    for c_index in range(0, len(encoded), step):
+        code = encoded[c_index:c_index+step]
+
+        d = int('0b' + code[:d_len].to01(), 2)
+        l = int('0b' + code[d_len:d_len + l_len].to01(), 2)
+
+        bin_next_sym = code[-8:].tobytes()
+        if bin_next_sym == b'\xff':
+            next_sym = ''
+        else:
+            next_sym = bin_next_sym.decode()
+
+        # print((d, l, next_sym))
+
+        substring = message[index - d:index - d + l] + next_sym
+        message += substring
+
+        index += l + 1
+
+    return message
+
+
 if __name__ == '__main__':
     # Test case from lecture slides
     test = "Peter Piper picked a peck of pickled peppers; A peck of pickled peppers Peter Piper picked;If Peter Piper picked a peck of pickled peppers,Where's the peck of pickled peppers Peter Piper picked?"
+    # test = 'ABRACADABRA'
+
+    # Fun one - d takes values of 2^i - 1 for i from 0 to log2(n)
+    # test = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+
+    window_size = buffer_size = len(test)
 
     uncompressed = bitarray.bitarray()
     uncompressed.fromstring(test)
 
-    compressed = compress(test)
+    compressed = compress(test, window_size, buffer_size)
 
     print(f'Uncompressed length: {uncompressed.length()}')
     print(f'Compressed length:   {compressed.length()}')
+
+    decompressed = decompress(compressed, window_size, buffer_size)
+
+    assert decompressed == test, 'Decompression invalid'
