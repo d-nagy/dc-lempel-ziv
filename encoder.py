9e95c7133d077739926e2a25bd5c47b090e25680
Daniel Nagy <daniel.nagy@durham.ac.uk>
2019-02-13 15:39:28 +0000

    Add LZSS encoder and decoder

--- encoder.py
+++ encoder.py
@@ -4,7 +4,7 @@ import os
 from bitarray import bitarray
 
 
-class LzEncoder():
+class Lz77Encoder():
     """ LZ77 Encoder """
 
     def __init__(self, window_size, buffer_size):
@@ -13,6 +13,7 @@ class LzEncoder():
         self.distance_bits = (window_size - 1).bit_length()
         self.length_bits = (buffer_size - 1).bit_length()
         self.compression = []
+        self.file_ext = '.LZ77'
 
 
     def set_window_size(self, window_size):
@@ -37,7 +38,7 @@ class LzEncoder():
 
         self.compression = []
 
-        with open(filename + '.LZIV', 'x') as output:
+        with open(filename + self.file_ext, 'x') as output:
             pass
 
         code_to_write = bitarray()
@@ -54,14 +55,14 @@ class LzEncoder():
             while buffer.length() > 0:
                 code = bitarray()
 
-                distance, length, next_sym = self._encode_at_pos(window, buffer)
-                code = self._code_to_bits(distance, length, next_sym)
+                distance, length, next_sym = self.encode_at_pos(window, buffer)
+                code = self.code_to_bits(distance, length, next_sym)
                 code_to_write += code
 
                 self.compression.append((distance, length, next_sym))
 
                 if not code_to_write.length() % 8:
-                    with open(filename + '.LZIV', 'ab') as output:
+                    with open(filename + self.file_ext, 'ab') as output:
                         output.write(code_to_write.tobytes())
                     code_to_write = bitarray()
 
@@ -78,13 +79,26 @@ class LzEncoder():
 
                 buffer = buffer[(length + 1) * 8:] + next_bytes
 
-        with open(filename + '.LZIV', 'ab') as output:
+        with open(filename + self.file_ext, 'ab') as output:
             output.write(code_to_write.tobytes())
 
         os.remove(filename)
 
 
-    def _code_to_bits(self, distance, length, next_sym):
+    def code_to_bits(self, distance, length, next_sym):
+        """
+        Encode a distance-length pair and the next character from the
+        LZ77 algorithm as binary.
+
+        Params:
+            distance: how many bytes backwards the match was found
+            length: the length of the found match
+            next_sym: the character following the matched substring
+
+        Returns:
+            Bitarray encoding the LZ77 code.
+        """
+
         distance_bin = bin(distance)[2:].rjust(self.distance_bits, '0')
         length_bin = bin(length)[2:].rjust(self.length_bits, '0')
 
@@ -97,7 +111,23 @@ class LzEncoder():
 
 
     @staticmethod
-    def _encode_at_pos(window, buffer):
+    def encode_at_pos(window, buffer):
+        """
+        Perform LZ77 coding at the current position in the input message,
+        characterized by the given sliding window and lookahead buffer.
+
+        Params:
+            window: the sliding window
+            buffer: the lookahead buffer
+
+        Returns:
+            A triple (distance, length, next_sym) where "distance" is the number
+            of bytes backwards in the message that a match was found for the
+            first "length" number of characters in the lookahead buffer, and
+            "next_sym" is the next character in the lookahead buffer after the
+            matched substring.
+        """
+
         window_bytes = window.tobytes()
         buffer_bytes = buffer.tobytes()
         buffer_len = len(buffer_bytes)
@@ -122,6 +152,86 @@ class LzEncoder():
         return (distance, length, next_sym)
 
 
+class LzssEncoder(Lz77Encoder):
+    """ LZSS Encoder """
+
+    def __init__(self, window_size, buffer_size):
+        super().__init__(window_size, buffer_size)
+        self.file_ext = '.LZSS'
+
+
+    def compress(self, filename):
+        """
+        Compress a file using LZSS coding.
+
+        Params:
+            filename: name of file to compress
+        """
+
+        self.compression = []
+
+        with open(filename + self.file_ext, 'x') as output:
+            pass
+
+        code_to_write = bitarray()
+
+        with open(filename, 'rb') as input_file:
+            buffer = bitarray()
+            window = bitarray()
+
+            try:
+                buffer.fromfile(input_file, self.buffer_size)
+            except EOFError:
+                buffer.fromfile(input_file)
+
+            while buffer.length() > 0:
+                code = bitarray()
+
+                distance, length, next_sym = self.encode_at_pos(window, buffer)
+                code = self.code_to_bits(distance, length, next_sym)
+                code_to_write += code
+
+                self.compression.append((distance, length, next_sym))
+
+                if not code_to_write.length() % 8:
+                    with open(filename + self.file_ext, 'ab') as output:
+                        output.write(code_to_write.tobytes())
+                    code_to_write = bitarray()
+
+                next_bytes = bitarray()
+                try:
+                    next_bytes.fromfile(input_file, length + 1)
+                except EOFError:
+                    pass
+
+                window += buffer[:(length + 1) * 8]
+
+                if window.length() > self.window_size * 8:
+                    del window[:window.length() - self.window_size * 8]
+
+                buffer = buffer[(length + 1) * 8:] + next_bytes
+
+        with open(filename + self.file_ext, 'ab') as output:
+            output.write(code_to_write.tobytes())
+
+        os.remove(filename)
+
+
+    def code_to_bits(self, distance, length, next_sym):
+        next_sym_bin = bitarray()
+        next_sym_bin.frombytes(next_sym)
+
+        if distance == 0 and length == 0:
+            code = bitarray('0') + next_sym_bin
+        else:
+            distance_bin = bin(distance)[2:].rjust(self.distance_bits, '0')
+            length_bin = bin(length)[2:].rjust(self.length_bits, '0')
+
+            code = bitarray('1' + distance_bin + length_bin) + next_sym_bin
+
+        return code
+
+
 if __name__ == '__main__':
     import sys
 
@@ -129,5 +239,8 @@ if __name__ == '__main__':
     W = int(sys.argv[2])
     L = int(sys.argv[3])
 
-    encoder = LzEncoder(W, L)
+    # encoder = Lz77Encoder(W, L)
+    encoder = LzssEncoder(W, L)
+
     encoder.compress(FILE)
+    # [print(code) for code in encoder.compression]
