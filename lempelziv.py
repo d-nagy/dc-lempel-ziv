5dac4b97e2f1889a3c919d2157f675207ea65fe4
Daniel Nagy <danielnagy69@gmail.com>
2019-02-14 22:49:56 +0000

    Reformat analysis methods and add comparison with gzip and bzip2

--- lempelziv.py
+++ lempelziv.py
@@ -3,6 +3,7 @@
 import bz2
 import gzip
 import os
+import shutil
 import time
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -24,24 +25,8 @@ class LempelZiv():
         self.lz77_decoder = decoder.Lz77Decoder(window_size, buffer_size)
 
 
-    def analyse_time_complexity(self, input_dir, rounds):
-        """
-        Perform a running time analysis on the encoder and decoder, plot and
-        save results.
-
-        Params:
-            input_dir: directory containing files to benchmark with
-            rounds: number of times to run benchmark on each file to get average
-                    times
-        """
-
-        files = [f'{input_dir}/{filename}' for filename in os.listdir(input_dir)]
-        files.sort(key=os.path.getsize)
-
-        file_sizes = {filename: os.path.getsize(filename) / 1000
-                      for filename in files}
-
-        benchmarks = {filename: self.benchmark_time(filename, rounds)
+    def get_time_complexity_results(self, files, rounds, alg=None):
+        benchmarks = {filename: self.benchmark_time(filename, rounds, alg)
                       for filename in files}
 
         avg_encoding_times = {key: value[0]['avg']
@@ -64,8 +49,17 @@ class LempelZiv():
 
         encoding_times = [avg_encoding_times, max_encoding_times, min_encoding_times]
         decoding_times = [avg_decoding_times, max_decoding_times, min_decoding_times]
-        labels = ['Average', 'Maximum', 'Minimum']
+
+        return encoding_times, decoding_times
+
+
+    def plot_encoder_time_complexity(self, encoding_times, files, file_sizes, gzip_times, bzip2_times):
+        labels = ['Average (LZ77)', 'Maximum (LZ77)', 'Minimum (LZ77)']
+
         colors = ['b', 'r', 'g']
+
+        plot_title = f'Encoder running time (window size = {self.window_size} bytes, buffer size = {self.buffer_size} bytes)'
+        plot_filename = 'plots/encoder_running_time.png'
 
         plt.cla()
 
@@ -77,19 +71,48 @@ class LempelZiv():
                 x_values.append(file_sizes[filename])
                 encoding_y_values.append(times[filename])
 
-            plt.plot(x_values, encoding_y_values, color + 'x', label=label)
+            plt.plot(x_values, encoding_y_values, color + 'x-', label=label)
 
-            fit = np.polyfit(x_values, encoding_y_values, 1)
-            fit_fn = np.poly1d(fit)
-            fit_range = np.arange(min(x_values), max(x_values))
-            plt.plot(fit_range, fit_fn(fit_range), color=color)
+        if gzip_times or bzip2_times:
+            plot_title = 'Encoding running time comparison'
+            plot_filename = 'plots/compare_encoding_running_time.png'
 
+        if gzip_times:
+            x_values = []
+            encoding_y_values = []
 
-        plt.title(f'Encoder running time (window size = {self.window_size} bytes, buffer size = {self.buffer_size} bytes)')
+            for filename in files:
+                x_values.append(file_sizes[filename])
+                encoding_y_values.append(gzip_times[0][filename])
+
+            plt.plot(x_values, encoding_y_values, 'yx-', label='Average (gzip)')
+
+        if bzip2_times:
+            x_values = []
+            encoding_y_values = []
+
+            for filename in files:
+                x_values.append(file_sizes[filename])
+                encoding_y_values.append(bzip2_times[0][filename])
+
+            plt.plot(x_values, encoding_y_values, 'cx-', label='Average (bzip2)')
+
+        plt.title(plot_title)
         plt.xlabel('File size (KB)')
         plt.ylabel('Running time (s)')
         plt.legend()
-        plt.savefig('plots/encoder_running_time.png')
+        plt.savefig(plot_filename)
+        plt.cla()
+
+
+    def plot_decoder_time_complexity(self, decoding_times, files, file_sizes, gzip_times, bzip2_times):
+        labels = ['Average (LZ77)', 'Maximum (LZ77)', 'Minimum (LZ77)']
+
+        colors = ['b', 'r', 'g']
+
+        plot_title = f'Decoder running time (window size = {self.window_size} bytes, buffer size = {self.buffer_size} bytes)'
+        plot_filename = 'plots/decoder_running_time.png'
+
         plt.cla()
 
         for times, label, color in zip(decoding_times, labels, colors):
@@ -100,18 +123,70 @@ class LempelZiv():
                 x_values.append(file_sizes[filename])
                 decoding_y_values.append(times[filename])
 
-            plt.plot(x_values, decoding_y_values, color + 'x', label=label)
+            plt.plot(x_values, decoding_y_values, color + 'x-', label=label)
 
-            fit = np.polyfit(x_values, decoding_y_values, 2)
-            fit_fn = np.poly1d(fit)
-            fit_range = np.arange(min(x_values), max(x_values))
-            plt.plot(fit_range, fit_fn(fit_range), color=color)
+        if gzip_times or bzip2_times:
+            plot_title = 'Decoding running time comparison'
+            plot_filename = 'plots/compare_decoding_running_time.png'
 
-        plt.title(f'Decoder running time (W = {self.window_size}, L = {self.buffer_size})')
+        if gzip_times:
+            x_values = []
+            encoding_y_values = []
+
+            for filename in files:
+                x_values.append(file_sizes[filename])
+                encoding_y_values.append(gzip_times[0][filename])
+
+            plt.plot(x_values, encoding_y_values, 'yx-', label='Average (gzip)')
+
+        if bzip2_times:
+            x_values = []
+            encoding_y_values = []
+
+            for filename in files:
+                x_values.append(file_sizes[filename])
+                encoding_y_values.append(bzip2_times[0][filename])
+
+            plt.plot(x_values, encoding_y_values, 'cx-', label='Average (bzip2)')
+
+        plt.title(plot_title)
         plt.xlabel('File size (KB)')
         plt.ylabel('Running time (s)')
         plt.legend()
-        plt.savefig('plots/decoder_running_time.png')
+        plt.savefig(plot_filename)
+        plt.cla()
+
+
+    def analyse_time_complexity(self, input_dir, rounds, compare=False):
+        """
+        Perform a running time analysis on the encoder and decoder, plot and
+        save results.
+
+        Params:
+            input_dir: directory containing files to benchmark with
+            rounds: number of times to run benchmark on each file to get average
+                    times
+        """
+
+        files = [f'{input_dir}/{filename}' for filename in os.listdir(input_dir)]
+        files.sort(key=os.path.getsize)
+
+        file_sizes = {filename: os.path.getsize(filename) / 1000
+                      for filename in files}
+
+        encoding_times, decoding_times = self.get_time_complexity_results(files, rounds)
+        gzip_encoding_times = gzip_decoding_times = []
+        bzip2_encoding_times = bzip2_decoding_times = []
+
+        self.plot_encoder_time_complexity(encoding_times, files, file_sizes, gzip_encoding_times, bzip2_encoding_times)
+        self.plot_decoder_time_complexity(decoding_times, files, file_sizes, gzip_decoding_times, bzip2_decoding_times)
+
+        if compare:
+            gzip_encoding_times, gzip_decoding_times = self.get_time_complexity_results(files, rounds, alg='gzip')
+            bzip2_encoding_times, bzip2_decoding_times = self.get_time_complexity_results(files, rounds, alg='bzip2')
+
+        self.plot_encoder_time_complexity(encoding_times, files, file_sizes, gzip_encoding_times, bzip2_encoding_times)
+        self.plot_decoder_time_complexity(decoding_times, files, file_sizes, gzip_decoding_times, bzip2_decoding_times)
 
 
     def analyse_time_params(self, filename, rounds):
@@ -253,7 +328,7 @@ class LempelZiv():
 
         axes.zaxis.set_major_locator(LinearLocator(10))
         axes.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-        axes.set_title(f'Compression Ratio (input = {os.path.getsize(filename)} bytes of Lorem Ipsum)')
+        axes.set_title(f'Compression Ratio (input = {os.path.getsize(filename)} bytes)')
         axes.set_xlabel('Window size (KB)')
         axes.set_ylabel('Lookahead buffer size (B)')
         axes.set_zlabel('Compression ratio')
@@ -267,14 +342,19 @@ class LempelZiv():
         axes.clear()
 
 
-    def analyse_file_types(self):
+    def analyse_file_types(self, compare=False):
         original_window_size = self.lz77_encoder.window_size
 
         main_dir = 'file_types'
 
         x_values = []
         ratios = []
-        mpl_colors = ['b', 'r', 'g', 'c', 'm', 'y', 'k']
+        gzip_ratios = []
+        bzip2_ratios = []
+        mpl_colors = ['lightsalmon', 'red', 'darkred', 'pink', 'deeppink', 'orange',
+                      'gold', 'lemonchiffon', 'darkkhaki', 'plum', 'magenta',
+                      'mediumpurple', 'purple', 'indigo', 'lime', 'seagreen',
+                      'aqua', 'black']
         colors = []
 
         i = 0
@@ -285,7 +365,7 @@ class LempelZiv():
 
             for filename in files:
                 uncompressed_size = os.path.getsize(filename)
-                window_size = min(uncompressed_size // 10, 15000)
+                window_size = min(150 * (uncompressed_size // 1000), 15000)
 
                 x_values.append(f'{round(uncompressed_size / 1000, 1)} KB {ext}')
                 colors.append(mpl_colors[i % len(mpl_colors)])
@@ -305,6 +385,27 @@ class LempelZiv():
 
                 ratios.append(ratio)
 
+                if compare:
+                    self.compress_gzip(filename)
+
+                    compressed_size = os.path.getsize(filename + '.gz')
+                    ratio = uncompressed_size / compressed_size
+                    print('Ratio (gzip): ', round(ratio, 2))
+
+                    self.decompress_gzip(filename + '.gz')
+
+                    gzip_ratios.append(ratio)
+
+                    self.compress_bzip2(filename)
+
+                    compressed_size = os.path.getsize(filename + '.bz2')
+                    ratio = uncompressed_size / compressed_size
+                    print('Ratio (bzip2): ', round(ratio, 2))
+
+                    self.decompress_bzip2(filename + '.bz2')
+
+                    bzip2_ratios.append(ratio)
+
             i += 1
 
         self.lz77_encoder.set_window_size(original_window_size)
@@ -313,17 +414,30 @@ class LempelZiv():
         x_pos = [i for i in range(len(x_values))]
 
         plt.cla()
+        fig = plt.figure(figsize=(12, 7))
+        fig.subplots_adjust(bottom=0.2)
+        plt.tight_layout()
         plt.bar(x_pos, ratios, color=colors)
         plt.xlabel('File')
         plt.ylabel('Compression ratio')
         plt.title('Compression ratio for various file types')
-        plt.xticks(x_pos, x_values, rotation='vertical')
-
+        plt.xticks(x_pos, x_values, rotation=90, fontsize='small')
         plt.savefig('plots/file_types.png')
         plt.cla()
 
+        if compare:
+            plt.bar(x_pos, ratios, 0.3, color='b', label='LZ77')
+            plt.bar(np.array(x_pos) + 0.3, gzip_ratios, 0.3, color='r', label='gzip')
+            plt.bar(np.array(x_pos) + 0.6, bzip2_ratios, 0.3, color='g', label='bzip2')
+            plt.xlabel('File')
+            plt.ylabel('Compression ratio')
+            plt.title('Compression ratio for various file types by compression algorithm')
+            plt.xticks(np.array(x_pos) + 0.45, x_values, rotation=90, fontsize='small')
+            plt.legend(loc='best')
+            plt.savefig('plots/ratio_comparison.png')
 
-    def benchmark_time(self, filename, rounds):
+
+    def benchmark_time(self, filename, rounds, alg=None):
         """
         Benchmark running time of compression and decompression on given file,
         obtaining the following data:
@@ -337,7 +451,23 @@ class LempelZiv():
             rounds: number of times to repeat benchmark
         """
 
-        print(f'{filename}: {rounds} rounds, W = {self.lz77_encoder.window_size}, L = {self.lz77_encoder.buffer_size}')
+        print(f'{filename}: {rounds} rounds')
+
+        if not alg:
+            print(f'W = {self.lz77_encoder.window_size}, L = {self.lz77_encoder.buffer_size}')
+            _compress = self.compress_lz77
+            _decompress = self.decompress_lz77
+            ext = self.lz77_encoder.file_ext
+        elif alg == 'gzip':
+            print('GZIP')
+            _compress = self.compress_gzip
+            _decompress = self.decompress_gzip
+            ext = '.gz'
+        elif alg == 'bzip2':
+            print('BZIP2')
+            _compress = self.compress_bzip2
+            _decompress = self.decompress_bzip2
+            ext = '.bz2'
 
         encoding_times = []
         decoding_times = []
@@ -346,12 +476,12 @@ class LempelZiv():
 
         for _ in range(rounds):
             encode_start = time.time()
-            self.compress_lz77(filename)
+            _compress(filename)
             encode_stop = time.time()
             encoding_times.append(encode_stop - encode_start)
 
             decode_start = time.time()
-            self.decompress_lz77(filename + self.lz77_encoder.file_ext)
+            _decompress(filename + ext)
             decode_stop = time.time()
             decoding_times.append(decode_stop - decode_start)
 
@@ -412,13 +542,49 @@ class LempelZiv():
 
 
     def compress_lz77(self, filename):
-        """ Compress data """
+        """ Compress data using LZ77 """
         self.lz77_encoder.compress(filename)
 
 
     def decompress_lz77(self, filename):
-        """ Decompress data """
+        """ Decompress data using LZ77 """
         self.lz77_decoder.decompress(filename)
+
+
+    def compress_gzip(self, filename):
+        """ Compress data with gzip """
+        with open(filename, 'rb') as input_file:
+            with gzip.open(filename + '.gz', 'wb') as output_file:
+                shutil.copyfileobj(input_file, output_file)
+
+
+    def decompress_gzip(self, filename):
+        """ Decompress data with gzip """
+        with gzip.open(filename, 'rb') as input_file:
+            with open(filename.replace('.gz', ''), 'wb') as output_file:
+                shutil.copyfileobj(input_file, output_file)
+
+        os.remove(filename)
+
+
+    def compress_bzip2(self, filename):
+        """ Compress data with bzip2 """
+        with open(filename, 'rb') as input_file:
+            data = input_file.read()
+            compressed = bz2.compress(data)
+            with open(filename + '.bz2', 'wb') as output_file:
+                output_file.write(compressed)
+
+
+    def decompress_bzip2(self, filename):
+        """ Decompress data with bzip2 """
+        with open(filename, 'rb') as input_file:
+            data = input_file.read()
+            decompressed = bz2.decompress(data)
+            with open(filename.replace('.bz2', ''), 'wb') as output_file:
+                output_file.write(decompressed)
+
+        os.remove(filename)
 
 
     @staticmethod
@@ -443,17 +609,13 @@ class LempelZiv():
 
 if __name__ == '__main__':
     INPUT_DIR = 'lorem'
-    FILE = 'lorem/60kb.txt'
+    FILE = 'misc/alice29.txt'
     W = 10000
     L = 100
 
     lz = LempelZiv(W, L)
 
-    # lz.analyse_time_complexity(INPUT_DIR, 5)
-    # lz.analyse_time_params('misc/alice29.txt', 10)
-    lz.analyse_file_types()
-    # lz.analyse_compression_ratio(FILE)
-
-    # print(len(lz.decoder.decompression))
-
-    # [print(f'{x}    {y}') for (x, y) in zip(lz.encoder.compression, lz.decoder.decompression)]
+    lz.analyse_time_complexity(INPUT_DIR, 10, compare=True)
+    lz.analyse_time_params('misc/alice29.txt', 10)
+    lz.analyse_file_types(compare=True)
+    lz.analyse_compression_ratio(FILE)
